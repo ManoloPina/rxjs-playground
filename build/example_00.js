72bda53b21bccb72198fcbdb6b61b6182a91a848
Manolo Pina <contato@manolopina.com.br>
2018-01-27 21:08:42 -0200

    Autocomplete

--- build/example_00.js
+++ build/example_00.js
@@ -5,5 +5,3 @@ var _moment = require('moment');
 var _moment2 = _interopRequireDefault(_moment);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-console.log((0, _moment2.default)().format());
